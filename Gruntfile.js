11c4b03ca0d3bef84e65c4ee86f42d6d007dc396
Bruner <asaf.bruner@sap.com>
2016-11-07 17:54:49 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {         
+    'use strict';
+    grunt.loadNpmTasks('grunt-sapui5-bestpractice-build');
+
+    grunt.registerTask('default', [
+        'clean',
+        'build'
+    ]);
+
+};
