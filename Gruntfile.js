7bffc18ea76e3efe19753a1bfb362edbc5a6b958
I074921 <asaf.bruner@sap.com>
2016-11-16 12:58:47 +0000

    Change the .eslint files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-sapui5-bestpractice-build');
 
     grunt.registerTask('default', [
+    	'lint',
         'clean',
         'build'
     ]);
